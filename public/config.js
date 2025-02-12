669c1fa60e2c974eb2d22de8a819a8ff8217af37
Kiana Kaslanna <130233273+JiangKaslana@users.noreply.github.com>
2025-02-12 09:54:54 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,8 +6,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2830288-cc52eb2dcb82c569b335738e',
-    'u2830288-00129cfa288e430d1a328235',
+    'm798557921-8eeebfe295f5488774c5a269',
+    'm798557917-0aaa02e13e9b310467ba4d30',
+    'm798557912-609d46bbfa615a235a32a297',
+    'm798564021-e259a69acb5eaac89fd20745',
+    'm798564028-5f2e2daf0a3233e2c8525651'
   ],
 
   // 日志天数
